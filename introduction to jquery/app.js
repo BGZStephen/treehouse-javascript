b2ccc18e1a71e08c43dbb11bff504ab6e0b873b1
BGZStephen <sjw948@gmail.com>
2017-02-18 09:07:22 +0000

    pet adoption app
    dom manipulation to add elements, remove elements and change css on
    click

--- introduction to jquery/app.js
+++ introduction to jquery/app.js
@@ -5,3 +5,27 @@ $(`.loc`).hover(
   function() {
     $(this).html(`<strong>Location:</strong> Treehouse Adoption Center`)
   });
+
+$(`#add-pet`).on(`click`, () => {
+  // grab info from the form
+  const $name = $(`#pet-name`).val();
+  const $species = $(`#pet-species`).val();
+  const $notes = $(`#pet-notes`).val();
+
+  const $newPet = $(
+     '<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $name +
+     '</p><p><strong>Species:</strong> ' + $species +
+     '</p><p><strong>Notes:</strong> ' + $notes +
+     '</p><span class="close">&times;</span></div></section>'
+  );
+
+  $(`#posted-pets`).append($newPet)
+})
+
+// images fade in
+
+$(`img`).css(`display`, `none`).fadeIn(1600);
+
+$(`.card`).on(`click`, function() {
+  $(this).toggleClass(`selected`);
+})
